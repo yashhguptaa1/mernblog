cf045abceeba2aa51e8159d25a0328d317d979a9
Yash Gupta <yashhguptaa1@gmail.com>
2021-09-23 17:54:41 +0530

    till l-21, user signup and added in db

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,37 @@
+const User = require('../models/user');
+const shortId = require('shortid');
+
 exports.signup = (req, res) => {
-  const { name, email, password } = req.body;
-  res.json({
-    user: { name, email, password },
-  });
+
+  //1. first we find if user exists
+    // console.log(req.body);
+    // we get this method from mongoose findOne : helps in finding particular user
+    User.findOne({ email: req.body.email }).exec((err, user) => {
+        if (user) {
+            return res.status(400).json({
+                error: 'Email is taken'
+            });
+        }
+
+  //2. create a new user
+        const { name, email, password } = req.body;
+        let username = shortId.generate();
+        let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+  
+  //3. save the new user
+        let newUser = new User({ name, email, password, profile, username });
+        newUser.save((err, success) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                });
+            }
+            // res.json({
+            //     user: success
+            // });
+            res.json({
+                message: 'Signup success! Please signin.'
+            });
+        });
+    });
 };
