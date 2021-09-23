171c981e5cbfe3adbebe96d8307ff78e43783a12
Yash Gupta <yashhguptaa1@gmail.com>
2021-09-23 21:04:04 +0530

    till lec-23, sigin implemented

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,9 @@
 const User = require('../models/user');
 const shortId = require('shortid');
 
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
+
 exports.signup = (req, res) => {
 
   //1. first we find if user exists
@@ -32,6 +35,34 @@ exports.signup = (req, res) => {
             res.json({
                 message: 'Signup success! Please signin.'
             });
+        });
+    });
+};
+
+
+exports.signin = (req, res) => {
+    const { email, password } = req.body;
+    // check if user exist
+    User.findOne({ email }).exec((err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User with that email does not exist. Please signup.'
+            });
+        }
+        // authenticate
+        if (!user.authenticate(password)) {
+            return res.status(400).json({
+                error: 'Email and password do not match.'
+            });
+        }
+        // generate a token and send to client
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+        res.cookie('token', token, { expiresIn: '1d' });
+        const { _id, username, name, email, role } = user;
+        return res.json({
+            token,
+            user: { _id, username, name, email, role }
         });
     });
 };
