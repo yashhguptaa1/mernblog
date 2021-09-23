26927a23c95f8250c2985c68a6e6e1a3a4752a9b
Yash Gupta <yashhguptaa1@gmail.com>
2021-09-24 00:55:15 +0530

    signup form created

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,13 +1,12 @@
 import Layout from "../components/Layout";
+import SignupComponent from "../components/auth/SignupComponent";
 import Link from "next/link";
 
 const Signup = () => {
   return (
     <Layout>
       <h2>Signup page</h2>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
+      <SignupComponent />
     </Layout>
   );
 };
